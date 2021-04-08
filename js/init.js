9df686c129cd2d6317b15e9e44d96b9a61066868
examen41cr <82001128+examen41cr@users.noreply.github.com>
2021-04-08 02:03:30 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyD_D9WhJvmIV3suWJoN_vQR9-u0wt_m2h8",
+    authDomain: "examen41cr-820f9.firebaseapp.com",
+    projectId: "examen41cr-820f9",
+    storageBucket: "examen41cr-820f9.appspot.com",
+    messagingSenderId: "826111510158",
+    appId: "1:826111510158:web:dbcc80d561ae3894f06bdf",
+    measurementId: "G-CC0FMXN1DY"
+});
